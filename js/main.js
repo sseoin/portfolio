016f176c84331e9df84f8b5c2e18a1451b0e7ed9
Danny Yang <yang_danny19@hotmail.com>
2018-01-18 23:59:05 -0800

    Clicking on the logo will bring the user back to the main page.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
   $("#main-div").load("partials/_work.html");
+  $(".navbar-brand").click(function () {
+    $("#main-div").load("partials/_work.html");
+  });
   $(".nav-item").click(function () {
     var page = $(this).data("page");
     $("#main-div").load("partials/_" + page + ".html");
